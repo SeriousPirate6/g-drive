44dde25a9019ecbd1942a64aa289bcea2e894174
SeriousPirate6 <whatyoudontexpect3@gmail.com>
2023-11-11 20:11:50 +0100

    Added parametric redirection handling in construct-url function.

--- src/g-drive/construct-url.js
+++ src/g-drive/construct-url.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const { handlingRedirects } = require("../utils/handling-redirections");
 
 module.exports = {
-  constructDriveUrl: async ({ web_link }) => {
+  constructDriveUrl: async ({ web_link, handle_redirects = false }) => {
     /*
      * extracting the sharable id
      */
-    return await handlingRedirects(
-      process.env.DRIVE_DOWNLOAD_URI + web_link.split("/")[5]
-    );
+    const direct_url = process.env.DRIVE_DOWNLOAD_URI + web_link.split("/")[5];
+
+    return handle_redirects ? await handlingRedirects(direct_url) : direct_url;
   },
 };
